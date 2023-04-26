a178e920c561678a3b4f24916ff5d518e9590df2
max.lehrke <max.lehrke@physec.de>
2023-04-26 15:00:14 +0200

    added logic to get values depending on user input

--- sql/SQLMain.py
+++ sql/SQLMain.py
@@ -3,8 +3,8 @@ import sqlite3
 from pathlib import Path
 import csv
 import datetime
-from typing import TextIO
-
+import numpy as np
+from time import sleep
 
 class SQLMain:
     """
@@ -22,7 +22,7 @@ class SQLMain:
         if not os.path.exists("./database.db"):
             Path('database.db').touch()
 
-    def open_csv_files(date) -> list:
+    def open_csv_files(self, date) -> list:
         if date != "2022-03-06" and date != "2022-10-29" and date != "2022-10-30":
             path = f"../core/utils/csv_files/{date}.csv"
             with open(path, 'r') as file:
@@ -42,13 +42,27 @@ class SQLMain:
             date = f"2022-{self.date.strftime('%m-%d')}"
             self.add_day()
             if date != "2022-03-06" and date != "2022-10-29" and date != "2022-10-30":
-                cur.executemany("INSERT INTO data (tstamp, P1, P2) VALUES (?, ?, ?);", self.open_csv_files(date))
+                cur.executemany("INSERT INTO data (tstamp, P1, P2) VALUES (?, ?, ?);", self.open_csv_files(date=date))
                 print(f"Adding {date}.csv to database...")
                 conn.commit()
 
         conn.close()
 
+    def get_values_from_dates(self,  value_type, start_date, end_date) -> list:
+        conn = sqlite3.connect(("../gui/database.db"))
+        cur = conn.cursor()
+        print("Loading...")
+        format_start_date = start_date.strftime("%Y-%m-%d")
+        format_end_date = end_date.strftime("%Y-%m-%d")
+        query = f'SELECT {value_type} FROM data WHERE date(tstamp) >= date(?) AND date(tstamp) <= date(?)'
+        cur.execute(query, (format_start_date, format_end_date))
+        result = [column[0] for column in cur.fetchall()]
+        sleep(0.5)
+        conn.close()
+        return result
+
+    def calculate_average(self, values: list) -> float:
+        return np.mean(values)
 
 sql = SQLMain()
 
-sql.add_csv_to_db()
